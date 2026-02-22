41ea888b1f5f8ed98cadf5b7f4b9d2bae0b5af73
Ahmed-gif123 <ahmh93700@gmail.com>
2026-02-22 19:08:30 +0500

    Update AuthFlow.h
    Signed-off-by: Ahmed-gif123 <ahmh93700@gmail.com>

--- launcher/minecraft/auth/AuthFlow.h
+++ launcher/minecraft/auth/AuthFlow.h
@@ -1,46 +1,46 @@
 #pragma once
 
-#include <QString>
-#include <memory>
+#include <QImage>
+#include <QList>
+#include <QObject>
+#include <QSet>
 
-class MinecraftAccount;
+#include "minecraft/auth/AccountData.h"
+#include "minecraft/auth/AuthStep.h"
+#include "tasks/Task.h"
 
-struct AuthSession {
-    bool MakeOffline(QString offline_playername);
-    void MakeDemo(QString name, QString uuid);
+class AuthFlow : public Task {
+    Q_OBJECT
 
-    QString serializeUserProperties();
+   public:
+    enum class Action { Refresh, Login, DeviceCode };
 
-    enum Status {
-        Undetermined,
-        RequiresOAuth,
-        RequiresPassword,
-        RequiresProfileSetup,
-        PlayableOffline,
-        PlayableOnline,
-        GoneOrMigrated
-    } status = Undetermined;
+    explicit AuthFlow(AccountData* data, Action action = Action::Refresh);
+    virtual ~AuthFlow() = default;
 
-    // combined session ID
-    QString session;
-    // volatile auth token
-    QString access_token;
-    // profile name
-    QString player_name;
-    // profile ID
-    QString uuid;
-    // 'legacy' or 'mojang', depending on account type
-    QString user_type;
-    // Did the auth server reply?
-    bool auth_server_online = false;
-    // Did the user request online mode?
-    bool wants_online = true;
+    void executeTask() override;
 
-    // Is this a demo session?
-    bool demo = false;
+    AccountTaskState taskState() { return m_taskState; }
 
-    // Does authentication confirm ownership of Minecraft?
-    bool ownMinecraft = true;
+   public slots:
+    bool abort() override;
+
+   signals:
+    void authorizeWithBrowser(const QUrl& url);
+    void authorizeWithBrowserWithExtra(QString url, QString code, int expiresIn);
+
+   protected:
+    void succeed();
+    void nextStep();
+
+   private slots:
+    // NOTE: true -> non-terminal state, false -> terminal state
+    bool changeState(AccountTaskState newState, QString reason = QString());
+    void stepFinished(AccountTaskState resultingState, QString message);
+
+   private:
+    AccountTaskState m_taskState = AccountTaskState::STATE_CREATED;
+    QList<AuthStep::Ptr> m_steps;
+    AuthStep::Ptr m_currentStep;
+    AccountData* m_data = nullptr;
 };
-
-using AuthSessionPtr = std::shared_ptr<AuthSession>;
