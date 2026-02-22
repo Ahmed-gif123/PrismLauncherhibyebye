02322bc1e2560d441d2d2f6217c7b44a03fe340b
Ahmed-gif123 <ahmh93700@gmail.com>
2026-02-22 17:24:18 +0500

    Remove setOfflineName method and offlineName member
    Removed setOfflineName method and related offlineName member.
    
    Signed-off-by: Ahmed-gif123 <ahmh93700@gmail.com>

--- launcher/LaunchController.h
+++ launcher/LaunchController.h
@@ -56,8 +56,6 @@ class LaunchController : public Task {
 
     void setOnline(bool online) { m_online = online; }
 
-    void setOfflineName(const QString& offlineName) { m_offlineName = offlineName; }
-
     void setDemo(bool demo) { m_demo = demo; }
 
     void setProfiler(BaseProfilerFactory* profiler) { m_profiler = profiler; }
@@ -77,7 +75,6 @@ class LaunchController : public Task {
     void launchInstance();
     void decideAccount();
     bool askPlayDemo();
-    QString askOfflineName(QString playerName, bool demo, bool* ok = nullptr);
     bool reauthenticateAccount(MinecraftAccountPtr account);
 
    private slots:
@@ -90,7 +87,6 @@ class LaunchController : public Task {
    private:
     BaseProfilerFactory* m_profiler = nullptr;
     bool m_online = true;
-    QString m_offlineName;
     bool m_demo = false;
     BaseInstance* m_instance;
     QWidget* m_parentWidget = nullptr;
